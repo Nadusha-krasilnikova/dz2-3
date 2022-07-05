300954819ef2af5f7bc3044e3296c2797426d457
Nadusha <nadusha230275@gmail.com>
2022-07-05 18:08:09 +0500

    работает если я правильно понела условия задачи

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// записать номер четверти, показать диапазон для возможных координат
+Console.WriteLine("введите номер четверти");
+int a= int.Parse(Console.ReadLine()?? "0");
+if (a==1)
+{
+    Console.WriteLine(" x>0 и y>0");
+}
+else if (a==2)
+{
+    Console.WriteLine(" x<0 и y>0");
+}
+else if (a==3)
+{
+    Console.WriteLine(" x<0 и y<0");
+}
+else if (a==4)
+{
+    Console.WriteLine(" x>0 и y<0");
+}
+else 
+{
+    Console.WriteLine("такой четверти не существует");
+}
